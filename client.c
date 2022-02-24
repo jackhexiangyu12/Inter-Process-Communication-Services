5f19ed424bc9ff14634eae07bb866f01e90693e6
Kyle <kwaldner3@gatech.edu>
2022-02-24 17:27:45 -0500

    revert client.c

--- client.c
+++ client.c
@@ -1,26 +1,7 @@
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <mqueue.h>
 #include <stdio.h>
+#include "client_library.h"
 
-void main(){
-    mqd_t mq_create, mq_snd_open;
-    int mq_ret, len;
-    //char buf[8192];
-
-    mq_snd_open = mq_open("/mymq2", O_WRONLY);
-    printf("please enter the message: ");
-    //scanf("%[^\n", buf);
-    char *buf = "my name is queue";
-
-    mq_ret = mq_send(mq_snd_open, buf, len+1, 0);
-    if (mq_ret == 0){
-        printf(" messeage que is working\n");
-    
-    }else{
-        printf("Message q not workingºny");
-    }
-
+int main() {
+  printf("this is the client\n");
+  print_stuff();
 }
