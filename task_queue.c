f46301152edd43f1c3f6b2888f3c3d59b099a1d8
Kyle <kwaldner3@gatech.edu>
2022-02-24 17:17:59 -0500

    added enqueue and print list functions

--- task_queue.c
+++ task_queue.c
@@ -9,3 +9,61 @@ active_q *get_active_q() {
   // nothing function just for filler
   return &q;
 }
+
+
+
+
+void print_list(int grab_lock) {
+  if (grab_lock)
+    pthread_mutex_lock(&q.lock);
+  task_node *curr = q.list_head;
+
+  if (curr == NULL) {
+    // printf("<<<<EMPTY LIST>>>\n");
+    printf("\n");
+    if (grab_lock)
+      pthread_mutex_unlock(&q.lock);
+    return;
+  }
+
+  while (curr != NULL) {
+
+    /* printf("%s ", curr->data); */
+
+    curr = curr->next;
+  }
+
+  printf("\n");
+  // printf("NIL\n");
+
+
+  if (grab_lock)
+    pthread_mutex_unlock(&q.lock);
+}
+
+void add_to_list(task_node *t_node) {
+  // should already have a lock on the list
+  if (q.list_head == NULL) {
+    // then set the lsit to the node
+    q.list_head = t_node;
+    // print_list(list, 0);
+    return;
+  }
+
+  if (q.list_head->next == NULL) {
+    q.list_head->next = t_node;
+    return;
+  }
+
+  // otherwise gotta find the end
+  task_node *curr = q.list_head;
+  while (curr->next != NULL) {
+    /* dprint("looping"); */
+    curr = curr->next;
+  }
+  // now curr is null
+
+  curr->next = t_node;
+  // print_list(list, 0);
+
+}
--- task_queue.h
+++ task_queue.h
@@ -1,7 +1,26 @@
-// linked list stuff here
+#include <pthread.h>
+
+typedef struct compression_task {
+  // idk what metadata this needs - should copy from uthreads
+  int stuff;
+} ctask;
+
+typedef struct task_list_node {
+  ctask *task;
+  struct task_list_node *next;
+} task_node;
+
+
 typedef struct __active_queue {
+
+  ctask *current_task;
+  int size; // size of the length of the linked list - dont count the current task
+  task_node *list_head;
+
+  pthread_mutex_t lock;
   int stub;
 } active_q;
 
 
 active_q *get_active_q();
+void print_list(int grab_lock);
