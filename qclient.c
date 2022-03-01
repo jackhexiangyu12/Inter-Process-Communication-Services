5a8eb93dbb7df8e0e80f25dd6a9b4c6222fd613d
Kyle <kwaldner3@gatech.edu>
2022-02-28 20:01:18 -0500

    basic shared memory proof of concept

--- qclient.c
+++ qclient.c
@@ -5,22 +5,46 @@
 #include <mqueue.h>
 #include <stdio.h>
 
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <wait.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/shm.h>
+
 void main(){
     mqd_t mq_create, mq_snd_open;
     int mq_ret, len;
-    //char buf[8192];
+    char buf[8192];
 
     mq_snd_open = mq_open("/mymq4", O_WRONLY);
-    /* printf("please enter the message: "); */
-    //scanf("%[^\n", buf);
-    char *buf = "my name is queue\n";
+
+    // make shared memory here
+    int segment_id = shmget(IPC_PRIVATE, 32, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
+
+    char *sh_mem = (char *) shmat(segment_id, NULL, 0);
+
+    /* printf("virtual address: %d\n", (int) sh_mem); */
+
+    char * text = "hello there\n";
+    memmove(sh_mem, text, strlen(text) + 1);
+
+
+
+    // right now I know the memory size is 32
+
+
+    sprintf(buf, "%d", segment_id);
     len = strlen(buf);
+    printf("%s\n", buf);
     mq_ret = mq_send(mq_snd_open, buf, len+1, 0);
+
     if (mq_ret == -1){
         printf(" messeage que is not working\n");
 
     }else{
-        printf("Message q is working");
+        printf("Message q is working\n");
     }
 
 }
--- recieve.c
+++ recieve.c
@@ -5,6 +5,14 @@
 #include <sys/stat.h>
 #include <mqueue.h>
 
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <wait.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/shm.h>
+
 void main(){
     mqd_t mq_create, mq_rec_open;
     int mq_ret, len;
@@ -13,7 +21,21 @@ void main(){
     mq_rec_open = mq_open("/mymq4", O_RDONLY);
     mq_ret = mq_receive(mq_rec_open, buf, sizeof(buf), NULL);
 
-    printf("message: %s\n",buf);
+    // buf now holds the string with a number
+
+    int segment_id = atoi(buf);
+
+    printf("segment id: %d\n", segment_id);
+
+    char *sh_mem = (char *) shmat(segment_id, NULL, 0);
+
+    /* printf("virtual address: %d\n", (int) sh_mem); */
+
+
+    printf("message: %s\n", sh_mem);
+
+    shmdt(sh_mem);
+    shmctl(segment_id, IPC_RMID, 0);
 
     if (mq_ret == -1){
         printf(" messeage que is not working\n");
